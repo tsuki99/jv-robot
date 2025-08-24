db2a6a8ddd75232bd3a651313f07f69d741e8c64
Vlad Rublenko <vladik.rar@ukr.net>
2025-08-24 19:30:06 +0300

    Implement RobotRoute logic to move robot to target coordinates

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,35 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        //write your solution here
+        changeDirection(true, robot.getX(), toX, robot);
+        moveToTargetX(robot, toX);
+        changeDirection(false, robot.getY(), toY, robot);
+        moveToTargetY(robot, toY);
+    }
+
+    private void changeDirection(boolean forX, int currentPos, int target, Robot robot) {
+        Direction trueDirection;
+        if (currentPos >= target) {
+            trueDirection = forX ? Direction.LEFT : Direction.DOWN;
+        } else {
+            trueDirection = forX ? Direction.RIGHT : Direction.UP;
+        }
+
+        do {
+            robot.turnLeft();
+        } while (!robot.getDirection().equals(trueDirection));
+    }
+
+    private void moveToTargetX(Robot robot, int target) {
+        do {
+            robot.stepForward();
+        } while (robot.getX() != target);
+    }
+
+    private void moveToTargetY(Robot robot, int target) {
+        do {
+            robot.stepForward();
+        } while (robot.getY() != target);
     }
 }
